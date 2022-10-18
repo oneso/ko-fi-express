6752f1e5db6001d5837c5feb35a5799c6b86b221
Marcel Görtz <mg@braincast.gmbh>
2022-10-18 11:59:46 +0200

    use @ko-fi/handler

--- index.ts
+++ index.ts
@@ -1,44 +1,15 @@
 import { Express, Request } from 'express';
 
-import { Config, mergeConfig, RequestData, Type } from '@ko-fi/types';
+import { kofiHandler } from '@ko-fi/handler';
+import { Config, mergeConfig } from '@ko-fi/types';
 
 export const kofi = (app: Express, config?: Partial<Config<Request>>) => {
-    const conf = mergeConfig(config);
+    const conf = mergeConfig({ endpoint: '/webhook', ...config });
 
-    app.post(conf.endpoint, async (req, res, next) => {
+    app.post(conf.endpoint, async (req, res) => {
         const { data } = req.body as { data: string; };
+        const status = await kofiHandler(data, conf, req);
 
-        try {
-            const parsed: RequestData = JSON.parse(data);
-
-            if (config.verificationToken && parsed.verification_token !== config.verificationToken) {
-                console.error('Ko-fi invalid verification token');
-                return res.sendStatus(401);
-            }
-
-            await conf.onData?.(parsed, req);
-
-            switch (parsed.type) {
-                case Type.Commission:
-                    await conf.onCommission?.(parsed, req);
-                    break;
-                case Type.Donation:
-                    await conf.onDonation?.(parsed, req);
-                    break;
-                case Type.ShopOrder:
-                    await conf.onShopOrder?.(parsed, req);
-                    break;
-                case Type.Subscription:
-                    await conf.onSubscription?.(parsed, req);
-                    break;
-            }
-        } catch (err) {
-            console.error('Ko-fi request error: ', err);
-            config.onError?.(err, req);
-
-            return res.sendStatus(400);
-        }
-
-        res.sendStatus(200);
+        res.sendStatus(status);
     });
 };
